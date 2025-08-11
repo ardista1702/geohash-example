2c1fa0b379ee546b1a31236bb4fa8c3e9ac1bae8
ardista1702 <ardist.1702@gmail.com>
2025-08-11 10:26:58 +0800

    refactoring chunck function

--- geohash_/geohash.go
+++ geohash_/geohash.go
@@ -20,53 +20,53 @@ func NewGeoHash(latitude, longitude float64, pressision uint8) *geohash {
 }
 
 func (gh *geohash) Encode() string {
-    totalBits := int(gh.Pressision * 5)
-    
-    lonBits := totalBits / 2
-    latBits := totalBits / 2
-    if totalBits%2 == 1 {
-        lonBits++ 
-    }
+	totalBits := int(gh.Pressision * 5)
 
-    latitude := gh.convertToBinary("latitude", uint8(latBits))
-    longitude := gh.convertToBinary("longitude", uint8(lonBits))
+	lonBits := totalBits / 2
+	latBits := totalBits / 2
+	if totalBits%2 == 1 {
+		lonBits++
+	}
 
-    interleaved := gh.interleaved(longitude, latitude)
-    chunck := gh.chunck(interleaved)
-    decimals := convertBinaryToDecimal(chunck)
-    return gh.hash(decimals)
+	latitude := gh.convertToBinary("latitude", uint8(latBits))
+	longitude := gh.convertToBinary("longitude", uint8(lonBits))
+
+	interleaved := gh.interleaved(longitude, latitude)
+	chunck := gh.chunck(interleaved)
+	decimals := convertBinaryToDecimal(chunck)
+	return gh.hash(decimals)
 }
 func (gh *geohash) convertToBinary(coordinate string, numBits uint8) []byte {
-    DefaultLatitude, DefaultLongitude := []float64{-90.0, 90.0}, []float64{-180.0, 180.0}
+	DefaultLatitude, DefaultLongitude := []float64{-90.0, 90.0}, []float64{-180.0, 180.0}
 
-    var coordinates []float64
-    var value float64
-    
-    result := make([]byte, numBits)
+	var coordinates []float64
+	var value float64
 
-    switch coordinate {
-    case "longitude":
-        coordinates = DefaultLongitude
-        value = gh.Longitude
-    case "latitude":
-        coordinates = DefaultLatitude
-        value = gh.Latitude
-    }
+	result := make([]byte, numBits)
 
-    left, right := coordinates[0], coordinates[1]
-    
-    // Loop sebanyak numBits, bukan gh.Pressision
-    for length := uint8(0); length < numBits; length++ {
-        mid := (left + right) / 2
-        if value > mid {
-            result[length] = 1
-            left = mid
-        } else {
-            result[length] = 0
-            right = mid
-        }
-    }
-    return result
+	switch coordinate {
+	case "longitude":
+		coordinates = DefaultLongitude
+		value = gh.Longitude
+	case "latitude":
+		coordinates = DefaultLatitude
+		value = gh.Latitude
+	}
+
+	left, right := coordinates[0], coordinates[1]
+
+	// Loop sebanyak numBits, bukan gh.Pressision
+	for length := uint8(0); length < numBits; length++ {
+		mid := (left + right) / 2
+		if value > mid {
+			result[length] = 1
+			left = mid
+		} else {
+			result[length] = 0
+			right = mid
+		}
+	}
+	return result
 }
 func (gh *geohash) interleaved(longBins, latBins []byte) []byte {
 	i, j := 0, 0
@@ -80,28 +80,27 @@ func (gh *geohash) interleaved(longBins, latBins []byte) []byte {
 	return result
 }
 func (gh *geohash) chunck(interleaved []byte) [][5]byte {
-	var res [][5]byte
-	for i := 0; i < len(interleaved); i += 5 {
-		var currentChunk [5]byte
-		for j := 0; j < 5; j++ {
-			if i+j < len(interleaved) {
-				currentChunk[j] = interleaved[i+j]
-			} else {
-				currentChunk[j] = 0 
-			}
+	numChunks := (len(interleaved) + 4) / 5
+	res := make([][5]byte, numChunks)
+
+	for i := 0; i < numChunks*5; i++ {
+		var bit byte = 0
+		if i < len(interleaved) {
+			bit = interleaved[i]
 		}
-		res = append(res, currentChunk)
+		res[i/5][i%5] = bit
+
 	}
 	return res
 }
 
 func (gh *geohash) hash(decimals []byte) string {
-base32Table := map[uint8]string{
-    0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7",
-    8: "8", 9: "9", 10: "b", 11: "c", 12: "d", 13: "e", 14: "f", 15: "g",
-    16: "h", 17: "j", 18: "k", 19: "m", 20: "n", 21: "p", 22: "q", 23: "r",
-    24: "s", 25: "t", 26: "u", 27: "v", 28: "w", 29: "x", 30: "y", 31: "z",
-}	
+	base32Table := map[uint8]string{
+		0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7",
+		8: "8", 9: "9", 10: "b", 11: "c", 12: "d", 13: "e", 14: "f", 15: "g",
+		16: "h", 17: "j", 18: "k", 19: "m", 20: "n", 21: "p", 22: "q", 23: "r",
+		24: "s", 25: "t", 26: "u", 27: "v", 28: "w", 29: "x", 30: "y", 31: "z",
+	}
 	var res []string
 	for _, dec := range decimals {
 		res = append(res, base32Table[dec])
@@ -132,5 +131,5 @@ func pow(base, exp byte) byte {
 		half := pow(base, exp/2)
 		return half * half
 	}
-	return base * pow(base,exp - 1)
+	return base * pow(base, exp-1)
 }

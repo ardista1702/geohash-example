8fde861060b0e7488cd88635ab55b988ff512384
ardista1702 <ardist.1702@gmail.com>
2025-08-11 10:21:44 +0800

    learn implementing geohash

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module geohash-example
 
 go 1.24.3
-
-require github.com/mmcloughlin/geohash v0.10.0
--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"fmt"
 	"geohash-example/geohash_"
-
-	"github.com/mmcloughlin/geohash"
 )
 
 func main() {
 	gh := geohash_.NewGeoHash(-6.1753924, 106.8271528, 12)
-	res := geohash.Encode(-6.1753924, 106.8271528)
-	fmt.Println(gh.Encode())
+	res := gh.Encode()
 	fmt.Println(res)
 }
